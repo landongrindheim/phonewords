18fc69e47176b9b636e46004d9470b7b9ce646e7
Landon Grindheim <landon.grindheim@gmail.com>
2017-06-30 11:47:05 -0400

    Add lookup test

---
+++ test/number_conversion/lookup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class LookupTest < Minitest::Test
+  def test_find_matches_returns_words_matching_pattern
+    pattern = /phone/
+    lookup = Phonewords::NumberConversion::Lookup.new(pattern: pattern)
+    result = lookup.find_matches
+    assert_includes result.map(&:chomp), "phone"
+  end
+
+  def test_find_matches_returns_multiple_matches
+    pattern = /words/
+    lookup = Phonewords::NumberConversion::Lookup.new(pattern: pattern)
+    result = lookup.find_matches
+    assert_includes result.map(&:chomp), "wordsmith"
+    assert_includes result.map(&:chomp), "backswordsman"
+    assert_includes result.map(&:chomp), "swordsman"
+  end
+
+  def test_find_matches_does_not_return_non_matching_strings
+    pattern = /phone/
+    lookup = Phonewords::NumberConversion::Lookup.new(pattern: pattern)
+    result = lookup.find_matches
+    refute_includes result.map(&:chomp), "words"
+  end
+end
