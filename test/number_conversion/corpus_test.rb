751cf44fb6caed67b5de2122beb6d970069eabee
Landon Grindheim <landon.grindheim@gmail.com>
2017-08-13 14:52:39 -0400

    Add new Corpus test for user given file

--- test/number_conversion/corpus_test.rb
+++ test/number_conversion/corpus_test.rb
@@ -1,6 +1,15 @@
 require "test_helper"
 
 class CorpusTest < Minitest::Test
+  def test_it_handles_a_user_given_words_file
+    fake_file = Minitest::Mock.new
+    fake_file.expect(:each_line, "This\n is\n my\n list\n")
+    File.stub(:new, fake_file) do
+      assert Phonewords::NumberConversion::Corpus.new(fake_file).each
+    end
+    fake_file.verify
+  end
+
   def test_it_implements_all_enumerable_methods
     corpus = Phonewords::NumberConversion::Corpus.new
 
