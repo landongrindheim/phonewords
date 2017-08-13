63630b59913ffb0b40523967c331326a4c0cadad
Landon Grindheim <landon.grindheim@gmail.com>
2017-08-12 22:49:38 -0400

    Allow Corpus to take a user-provided words file

--- lib/phonewords/number_conversion/corpus.rb
+++ lib/phonewords/number_conversion/corpus.rb
@@ -3,13 +3,17 @@ module Phonewords
     class Corpus
       include Enumerable
 
+      def initialize(words_file=default_words_file)
+        @words_file = words_file
+      end
+
       def each(&block)
-        File.new(available_words_file).each_line(&block)
+        File.new(@words_file).each_line(&block)
       end
 
       private
 
-      def available_words_file
+      def default_words_file
         return primary if File.exist? primary
         return secondary if File.exist? secondary
         raise "No Dictionary Available"
